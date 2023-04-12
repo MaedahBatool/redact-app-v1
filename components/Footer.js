df335333718c2ee8f5a792bb6ed13207537e5568
Maedah Batool <maedahbatool@192.168.1.3>
2023-04-12 20:12:28 +0500

    Fix typo

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,6 @@ export default function Footer() {
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
             Docs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
             </span>
           </h2>
           <p
